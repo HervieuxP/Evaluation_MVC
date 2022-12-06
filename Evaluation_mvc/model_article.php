3a1b99fddfcc2bedfc154a8b51951b535c109155
HervieuxP <108793059+HervieuxP@users.noreply.github.com>
2022-12-06 10:01:47 +0100

    update_model_article

--- Evaluation_mvc/model_article.php
+++ Evaluation_mvc/model_article.php
@@ -13,22 +13,8 @@ try{
     //Message de validation
     echo"<p>L'article a bien été enregistré.</p>";
 
-    //Nouvelle requête pour récupérer l'id_user enregistré
-    $req = $bdd->prepare('select id_vendeur from ticket where id_vendeur = ?');
-    //Binding de Paramètre -> j'injecte les infos de mon user dans la requête
-    $req->bindParam(1,$id_vendeur,PDO::PARAM_STR);
-    //Exécution de la requête
-    $req->execute();
-
-    //stocke la réponse de la BDD
-    $data = $req->fetchAll();
-
-    //j'extrait la donnée que je veux, depuis mon tableau de tableaux associatifs $data
-    $id_user;
-    foreach($data as $row){
-        $id_vendeur = $row['id_vendeur'];
-    }
-
+    $req=null;
+    $bdd=null;
 
 }catch(Exception $error){
     //affichage d'une exception en cas d’erreur
